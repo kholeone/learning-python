7061d0034ee222ab9b817065726d09c4b274d63c
kholeo taylor <kholeotaylor@hotmail.co.uk>
2020-10-28 15:35:56 +0000

    initial commit

--- tests/test_factorial.py
+++ tests/test_factorial.py
@@ -1,6 +1,9 @@
 import pytest
 from programs import factorial
 
-def test_factorial_variable():
+def test_factorial_1():
     assert factorial.fact(0) == 1
+
+def test_factorial_2():
+    assert factorial.fact(3) == 6
 
